9fc170c764ca31309cc5ccf4166fe9b6381f9a1f
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-14 13:54:29 -0700

    changes on application html

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,14 @@
 class WorksController < ApplicationController
+  
+  # def create 
+  #   @work = Work.new(work_params)
+  #   if @work.save
+  #     redirect_to root_path
+  #     return
+  #     flash[:success] = "Successfully created #{@work.category}#{@work.title}"
+  #   else 
+  #     render :new, :status, :bad_request        
+  #     return 
+  #   end 
+  # end 
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,5 @@
 class Work < ApplicationRecord
+  has_many :votes #TODO how can the id be unique
   validates :title, presence: true, uniqueness: true 
+  
 end
