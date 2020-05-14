7385a766eadde3f234c2cabb3806559f55f3a8f4
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-13 19:06:33 -0700

    setting up user controller and adding TODO comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'homepage#index'
+  root to: 'homepage#index'
+  
+  resources :users
+  
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+  
+  resources :works
+  
+  
   
 end
