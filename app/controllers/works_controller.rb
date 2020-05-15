ea6d2f88ed2d8e11800a08d241ef64ec59310572
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-14 19:25:56 -0700

    main page almost done

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,15 +2,17 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end 
-  # def create 
-  #   @work = Work.new(work_params)
-  #   if @work.save
-  #     redirect_to root_path
-  #     return
-  #     flash[:success] = "Successfully created #{@work.category}#{@work.title}"
-  #   else 
-  #     render :new, :status, :bad_request        
-  #     return 
-  #   end 
-  # end 
+  def 
+  end 
+  def create 
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to root_path
+      return
+      flash[:success] = "Successfully created #{@work.category}#{@work.title}"
+    else 
+      render :new, :status, :bad_request        
+      return 
+    end 
+  end 
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,6 @@ class Work < ApplicationRecord
   has_many :votes #TODO how can the id be unique
   validates :title, presence: true, uniqueness: true 
   
+  #TODO get the 10 most voted from each type 
+  
 end
