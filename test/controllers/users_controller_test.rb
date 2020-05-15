24d7dd2f67a0fc140144c631d543ed6ef2b329cf
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-15 13:19:02 -0700

    some login tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "can get the login form" do 
+    get login_path
+    
+    must_respond_with :success
+  end 
+  
+  describe "Logging in" do 
+    it "can log in a new user" do 
+      user = nil #to existe in the whole block, declare outside
+      expect {
+        user = login()
+      }.must_differ "User.count", 1
+      
+      must_respond_with :redirect
+      
+      expect(user).wont_be_nil
+      expect(session[:user_id]).must_equal user.id
+      expect(user.username).must_equal user_hash[:user][:username]
+    end 
+    
+    it "can login an existing user" do 
+      user = User.create(username: "Ed Sheeran")
+      
+      expect {
+        login(user.username)
+      }.wont_change "User.count"
+      expect(session[:user_id]).must_equal user.id
+    end 
+    
+  end 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,21 +1,31 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-      require "minitest/rails"
-      require "minitest/reporters"  # for Colorized output
-      #  For colorful output!
-      Minitest::Reporters.use!(
-        Minitest::Reporters::SpecReporter.new,
-        ENV,
-        Minitest.backtrace_filter
-      )
+require "minitest/rails"
+require "minitest/reporters"  # for Colorized output
+#  For colorful output!
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   # parallelize(workers: :number_of_processors) # causes out of order output.
-
+  
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  def login(username = "Grace Hopper")
+    user_hash = {
+      user: {
+        username: username
+      }
+    }
+    post login_path, params: user_hash
+    
+    user = User.find_by(username: username)
+    return user
+  end 
   # Add more helper methods to be used by all tests here...
 end
