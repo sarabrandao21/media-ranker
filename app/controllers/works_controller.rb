41ba8d1e94d97745998ede13586334ea38264b80
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-14 21:10:51 -0700

    show details page for each media

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,17 +2,32 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end 
-  def 
+  
+  def show 
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+    if @work.nil?
+      head :not_found 
+      return
+    end
+    
+  end 
+  
+  def new 
+    @work = Work.new
   end 
   def create 
     @work = Work.new(work_params)
     if @work.save
-      redirect_to root_path
+      redirect_to works_path
       return
       flash[:success] = "Successfully created #{@work.category}#{@work.title}"
     else 
-      render :new, :status, :bad_request        
+      render :new, status: :bad_request       
       return 
     end 
   end 
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
