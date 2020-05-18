fc401fa5bb87cc690e25e3a97796290084c71def
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-17 20:14:18 -0700

    tests for spotlight

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 describe Vote do
-  
+  describe "relations" do 
+    it "can have user and work" do 
+      new_work = Work.create(category: "album",title: "Blue Break",creator: "Dr. Sarai Langosh",publication_year: "1949", description: "Et et expedita non aut quo.")
+      user_one = User.create(username: "benilsen")
+      vote = Vote.create(work_id: new_work.id, user_id: user_one.id)
+      expect(vote.work_id).must_equal new_work.id
+      expect(vote.user_id).must_equal user_one.id
+    end 
+  end 
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 describe Work do
   before do 
-    @new_work = Work.new(category: "album",title: "Blue Breaker",creator: "Dr. Sarai Langosh",publication_year: "1949", description: "Et et expedita non aut quo.")
+    @new_work = Work.new(category: "album",title: "Blue Break",creator: "Dr. Sarai Langosh",publication_year: "1949", description: "Et et expedita non aut quo.")
   end 
   describe 'validations' do 
     
@@ -37,17 +37,26 @@ describe Work do
   end 
   describe "Spotlight" do 
     it "returns 10 works" do 
-      
+      list_work = Work.spotlight("album")
+      expect(list_work.length).must_equal 10
     end 
     it "does not return more than 10 works" do 
-    end 
-    it "return a list of works sorted by highest votes" do 
+      @new_work.save #adding one more work total is 11 
+      list_work = Work.spotlight("album")
+      expect(list_work.length).must_equal 10 
     end 
     it "should return a list with works of the given category" do 
+      list_work = Work.spotlight("album")
+      list_work.each do |work|
+        expect(work.category).must_equal "album"
+      end 
     end 
   end 
   describe "Create new vote" do 
     it "vote can be created" do 
+      user_one = User.create(username: "benilsen")
+      works(:work_one).create_new_vote(user_one.id)
+      expect(user_one.votes.length).must_equal 1 
     end 
   end 
 end 
